3c651c24a9d41339e6bb7d2bc77fef10f7bb9d62
vinay-osseed <vinay@osseed.com>
2022-05-12 23:06:28 +0530

    write comments to `js` code.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+/* Listening for a change in the `user_type` element and when it changes it sets the `Developer`
+element to `none` and the `General` element to `block` if the `user_type` element is equal to
+`General`. */
 document.getElementById('user_type').addEventListener('change',(selected_value)=>{
   if (selected_value.target.value === 'General') {
     document.getElementById('Developer').style.display = 'none';
@@ -12,6 +15,9 @@ document.getElementById('user_type').addEventListener('change',(selected_value)=
     document.getElementById('Developer').style.display = 'none';
   }
 });
+
+/* Listening for a change in the `src_photo` element and when it changes it sets the `dest_photo`
+element to the value of the `src_photo` element. */
 document.getElementById('src_photo').addEventListener('change',(photo)=>{
 	document.getElementById('dest_photo').src = URL.createObjectURL(photo.target.files[0]);
 });
