ebc7424ecf6a7eca31f39e11eb1af024d50585ee
vinugawade <vinulike11@gmail.com>
2023-08-25 20:23:59 +0530

    minor: remove `line-clamp` plugin from tailwind.

--- tailwind.config.js
+++ tailwind.config.js
@@ -950,6 +950,5 @@ module.exports = {
   plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
     require('@tailwindcss/aspect-ratio'),],
 }
