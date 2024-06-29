65d97349b5c849a036e2202e303f37143b7c4fc3
vinay-osseed <vinay@osseed.com>
2024-06-29 12:11:01 +0530

    feat: add `social card` modal with all

---
+++ app/Http/Controllers/SocialCardController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoresocialCardRequest;
+use App\Http\Requests\UpdatesocialCardRequest;
+use App\Models\socialCard;
+
+class SocialCardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoresocialCardRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(socialCard $socialCard)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(socialCard $socialCard)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatesocialCardRequest $request, socialCard $socialCard)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(socialCard $socialCard)
+    {
+        //
+    }
+}
