3c232d4029d8b4f26264a76d059ed1572d0ca7f3
vinay-osseed <vinay@osseed.com>
2024-06-29 11:56:00 +0530

    feat: update to `redirect_after_auth` settings

--- config/devdojo/auth/settings.php
+++ config/devdojo/auth/settings.php
@@ -4,7 +4,7 @@
  * These are some default authentication settings
  */
 return [
-    'redirect_after_auth' => '/',
+    'redirect_after_auth' => '/dashboard',
     'registration_show_password_same_screen' => true,
     'registration_include_name_field' => true,
     'registration_require_email_verification' => false,
